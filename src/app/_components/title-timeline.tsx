d1d076ba53b2f32b6bb17d4fc28008c9644f6e25
Dharma Yudistira <dharmayudistira2000@gmail.com>
2025-01-18 22:53:47 +0700

    fix

--- src/app/_components/title-timeline.tsx
+++ src/app/_components/title-timeline.tsx
@@ -2,9 +2,9 @@
 
 export const TitleTimeline = () => {
   return (
-    <div className="flex h-[20vh] w-full justify-center bg-white md:h-[50vh]">
+    <div className="flex h-full w-full justify-center bg-white md:h-[35vh]">
       <div className="relative h-full w-[90vw]">
-        <div className="absolute bottom-0 left-0 right-0 top-0 md:top-12">
+        <div className="absolute bottom-0 left-0 right-0 top-0 md:top-0">
           <p
             className="text-nowrap pl-8 pt-10 text-2xl font-futuraBold text-[#061936] md:text-[112px]"
             style={{ lineHeight: 1 }}
@@ -20,7 +20,7 @@ export const TitleTimeline = () => {
           </p>
         </div>
 
-        <div className="absolute bottom-0 left-0 right-0 top-8 md:top-10">
+        <div className="absolute bottom-0 left-0 right-0 top-8 md:top-0">
           <p
             className="text-nowrap pr-0 pt-0 text-end text-5xl font-bold text-[#061936]/10 md:text-[250px]"
             style={{ lineHeight: 1 }}
